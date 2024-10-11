e9762f3ef9265e24f6a6ac999a16730b820a2682
watawada <k3mai@ucsd.edu>
2024-10-11 00:22:28 -0700

    Wave bullet
    created bullet that moves in a sin wave path

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    enum BulletTypes { Normal, Bounce }
+
+    enum BulletTypes { Normal, Bounce, Wave }
+
+    // Variables for all bullet types
     [SerializeField] private BulletTypes bulletType;
     public float movespeed = 5f;
     public float bulletLife = 8f;
     public Vector3 bulletDirection = new Vector3(0,0,0);
     private float timer = 0f;
 
+    // Variables for Bounce type
     public int bounceCap;
     private int bounceCount = 0;
+
+    // Variables for Wave type
+    public float amplitude;
+    public float frequency;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if(bulletType == BulletTypes.Normal && timer >= bulletLife)
+        if(bulletType != BulletTypes.Bounce && timer >= bulletLife)
         {
             Destroy(gameObject);
             timer = 0f;
@@ -32,6 +40,13 @@ public class BulletScript : MonoBehaviour
         {
             Destroy(gameObject);
             bounceCount = 0;
+        } else if(bulletType == BulletTypes.Wave)
+        {
+            Vector3 perpendicular = new Vector3(bulletDirection.y, -bulletDirection.x, 0);
+            Vector3 moveY = perpendicular * Mathf.Sin(Time.time * frequency) * amplitude;
+
+            transform.position += (moveY + bulletDirection) * movespeed * Time.deltaTime;
+            
         }
 
         transform.position += movespeed * Time.deltaTime * bulletDirection;
@@ -39,11 +54,12 @@ public class BulletScript : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (bulletType == BulletTypes.Normal)
+        Debug.Log("collision");
+        if (bulletType != BulletTypes.Bounce && collision.gameObject.tag != "Bullet")
         {
             Destroy(gameObject);
         }
-        if (bulletType == BulletTypes.Bounce)
+        if (bulletType == BulletTypes.Bounce && collision.gameObject.tag != "Bullet")
         {
             if (collision.gameObject.name == "player")
             {
