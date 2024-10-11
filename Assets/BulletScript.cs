d30a67040747580e36093ad8a802e7cd84f9edac
watawada <k3mai@ucsd.edu>
2024-10-10 18:40:48 -0700

    bouncy bullet
    created bullet type that bounces off non-player objects

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    enum BulletTypes { Normal, Bounce }
+    [SerializeField] private BulletTypes bulletType;
     public float movespeed = 5f;
     public float bulletLife = 8f;
     public Vector3 bulletDirection = new Vector3(0,0,0);
     private float timer = 0f;
+
+    public int bounceCap;
+    private int bounceCount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +24,33 @@ public class BulletScript : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= bulletLife)
+        if(bulletType == BulletTypes.Normal && timer >= bulletLife)
         {
             Destroy(gameObject);
             timer = 0f;
+        } else if(bulletType == BulletTypes.Bounce && bounceCount >= bounceCap)
+        {
+            Destroy(gameObject);
+            bounceCount = 0;
         }
 
         transform.position += movespeed * Time.deltaTime * bulletDirection;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (bulletType == BulletTypes.Normal)
+        {
+            Destroy(gameObject);
+        }
+        if (bulletType == BulletTypes.Bounce)
+        {
+            if (collision.gameObject.name == "player")
+            {
+                Destroy(gameObject);
+            }
+            bulletDirection = Vector3.Reflect(bulletDirection, collision.contacts[0].normal);
+            bounceCount += 1;
+        }
     }
 }
